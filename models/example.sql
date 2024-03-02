e6956f6c3eb8829405f558e107d62c922d04221d
Aaron Williams <aaron@trinary.llc>
2024-03-01 22:02:16 -0800

    working example

--- models/example.sql
+++ models/example.sql
@@ -1,2 +1,19 @@
 {{ config(MATERIALIZED = 'external', location = 'output/test', format='csv') }}
-SELECT * FROM {{ ref('raw_artist') }}
+with source as (
+
+    {#-
+    Normally we would select from the table here, but we are using seeds to load
+    our data in this project
+    #}
+
+    select * from {{ ref('raw_artist') }}
+),
+
+renamed AS (
+    SELECT
+       artist_id,
+       name AS artist_name
+    FROM source
+)
+
+SELECT * FROM renamed
